f94facd36a53cfd5b3adf2daf20848e5d1f7bab7
unknown <samuelsantosoliveira116@gmail.com>
2022-04-25 20:33:12 -0300

    initial code

---
+++ Assets/Scripts/MeuScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("BEM VINDO");
+        print("SEU PORRA");     
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
